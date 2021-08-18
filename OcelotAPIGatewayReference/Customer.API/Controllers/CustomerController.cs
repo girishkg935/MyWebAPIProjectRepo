559ea47043c2d885f99e989501b3a1db0c4df395
girishkg935 <girishkg935@gmail.com>
2021-08-18 12:06:48 +0530

    This is the first commit after having created customer service

---
+++ OcelotAPIGatewayReference/Customer.API/Controllers/CustomerController.cs
@@ -0,0 +1,66 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomerService.Models;
+
+// For more information on enabling Web API for empty projects, visit https://go.microsoft.com/fwlink/?LinkID=397860
+
+namespace CustomerService.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CustomerController : ControllerBase
+    {
+        private readonly CustomerDbContext context;
+
+        public CustomerController(CustomerDbContext context)
+        {
+            this.context = context;
+        }
+
+        // GET: api/<UserController>
+        [HttpGet]
+       public IEnumerable<Models.Customer> Get()
+        {
+            return context.Customers.ToList();
+        }
+
+        // GET api/<UserController>/5
+        [HttpGet("{id}")]
+        public Models.Customer Get(int id)
+        {
+            return context.Customers.Find(id);
+        }
+
+        // POST api/<UserController>
+        [HttpPost]
+        public IActionResult Post([FromBody] Models.Customer customer)
+        {
+            try
+            {
+                context.Customers.Add(customer);
+                context.SaveChanges();
+
+                return StatusCode(200, customer);
+            }
+            catch(Exception ex)
+            {
+                return StatusCode(500,ex);
+            }
+        }
+
+        // PUT api/<UserController>/5
+        [HttpPut("{id}")]
+        public void Put(int id, [FromBody] string value)
+        {
+        }
+
+        // DELETE api/<UserController>/5
+        [HttpDelete("{id}")]
+        public void Delete(int id)
+        {
+        }
+    }
+}
---
+++ OcelotAPIGatewayReference/Customer.API/Models/CustomerDbContext.cs
@@ -0,0 +1,22 @@
+﻿using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using CustomerService.Models;
+
+namespace CustomerService.Models
+{
+    public class CustomerDbContext : DbContext
+    {
+
+        public DbSet<Customer> Customers { get; set; }
+
+        protected override void OnConfiguring(DbContextOptionsBuilder optionsBuilder)
+        {
+
+            optionsBuilder.UseSqlServer(@"data source=bluesapphire\sqlexpress;Initial Catalog=CustomerMicroservice;Integrated Security=True;");
+
+        }
+    }
+}
